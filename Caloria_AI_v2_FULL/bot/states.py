2f3ec08c4109939b9a5e0e6297fe9246cf9aae45
Ukt21 <141407148+Ukt21@users.noreply.github.com>
2025-11-27 23:53:14 +0500

    Define user registration states in states.py
    Added a RegisterStates class to define user registration states.

--- Caloria_AI_v2_FULL/bot/states.py
+++ Caloria_AI_v2_FULL/bot/states.py
@@ -1,1 +1,12 @@
 # placeholder states
+from aiogram.fsm.state import State, StatesGroup
+
+class RegisterStates(StatesGroup):
+    gender = State()
+    age = State()
+    weight = State()
+    height = State()
+    activity = State()
+    goal = State()
+    nutrition = State()
+    allergies = State()
