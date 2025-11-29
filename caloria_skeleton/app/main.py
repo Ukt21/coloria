ebe003e5c7cd73ff679561c756e2fd3d64481e7c
Ukt21 <141407148+Ukt21@users.noreply.github.com>
2025-11-29 15:14:48 +0500

    Update main.py

--- caloria_skeleton/app/main.py
+++ caloria_skeleton/app/main.py
@@ -1,8 +1,25 @@
-
 import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+
+from app.config import settings
+from app.handlers.start import start_router
+
 
 async def main():
-    print("Caloria bot starting... placeholder.")
+    logging.basicConfig(level=logging.INFO)
 
-if __name__ == '__main__':
+    bot = Bot(token=settings.BOT_TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+
+    # Регистрация роутеров
+    dp.include_router(start_router)
+
+    print("Bot started...")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
     asyncio.run(main())
